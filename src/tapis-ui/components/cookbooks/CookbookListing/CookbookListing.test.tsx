1c677282d6370b85cae046a412a31de44de8666d
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-14 11:38:27 -0400

    fix: add unit tests

--- src/tapis-ui/components/cookbooks/CookbookListing/CookbookListing.test.tsx
+++ src/tapis-ui/components/cookbooks/CookbookListing/CookbookListing.test.tsx
@@ -1,108 +1,108 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import renderComponent from 'utils/testing';
-import FileListing from './CookbookListing';
-import { useList } from 'tapis-hooks/files';
-import { fileInfo } from 'fixtures/files.fixtures';
-import { Files } from '@tapis/tapis-typescript';
+import CookbookListing from './CookbookListing';
+import { tapisApp } from 'fixtures/apps.fixtures';
+import { Apps } from '@tapis/tapis-typescript';
+import { useList } from 'tapis-hooks/apps';
 
-jest.mock('tapis-hooks/files');
+jest.mock('tapis-hooks/apps');
 
-describe('Files', () => {
-  it('renders File Listing component', () => {
+describe('Cookbooks', () => {
+  it('renders Cookbook Listing component', () => {
     (useList as jest.Mock).mockReturnValue({
-      concatenatedResults: [fileInfo],
+      data: {
+        result: [tapisApp],
+      },
       isLoading: false,
       error: null,
     });
-    const { getAllByText } = renderComponent(
-      <FileListing systemId={'system'} path={'/'} />
-    );
-    expect(getAllByText(/file1/).length).toEqual(1);
-    expect(getAllByText(/01\/01\/2020/).length).toEqual(1);
-    expect(getAllByText(/29.3 kB/).length).toEqual(1);
+    const { getAllByText } = renderComponent(<CookbookListing />);
+    expect(getAllByText(/FullJobAttrs/).length).toEqual(1);
+    // expect(getAllByText(/01\/01\/2020/).length).toEqual(1);
+    // expect(getAllByText(/29.3 kB/).length).toEqual(1);
   });
 
-  it('performs file selection', () => {
+  it('performs cookbook selection', () => {
     (useList as jest.Mock).mockReturnValue({
-      concatenatedResults: [{ ...fileInfo }],
+      data: {
+        result: [tapisApp],
+      },
       isLoading: false,
       error: null,
     });
     const mockOnSelect = jest.fn();
     const { getByTestId } = renderComponent(
-      <FileListing
-        systemId={'system'}
-        path={'/'}
-        selectMode={{ mode: 'single', types: ['dir', 'file'] }}
+      <CookbookListing
+        selectMode={{ mode: 'single', types: ['dir', 'app'] }}
         onSelect={mockOnSelect}
       />
     );
     // Find the file1.txt and file2.txt rows
-    const file1 = getByTestId('file1.txt');
-    expect(file1).toBeDefined();
+    const app1 = getByTestId('FullJobAttrs');
+    expect(app1).toBeDefined();
 
     // Click on file1.txt and expect the select callback to have run
-    file1.click();
-    expect(mockOnSelect).toHaveBeenLastCalledWith([fileInfo]);
+    app1.click();
+    expect(mockOnSelect).toHaveBeenLastCalledWith([tapisApp]);
   });
 
   it('performs file unselection', () => {
     (useList as jest.Mock).mockReturnValue({
-      concatenatedResults: [{ ...fileInfo }],
+      data: {
+        result: [tapisApp],
+      },
       isLoading: false,
       error: null,
     });
     const mockOnUnselect = jest.fn();
     const { getByTestId } = renderComponent(
-      <FileListing
-        systemId={'system'}
-        path={'/'}
-        selectMode={{ mode: 'single', types: ['dir', 'file'] }}
-        selectedFiles={[fileInfo]}
+      <CookbookListing
+        selectMode={{ mode: 'single', types: ['dir', 'app'] }}
+        selectedApps={[tapisApp]}
         onUnselect={mockOnUnselect}
       />
     );
     // Find the file1.txt and file2.txt rows
-    const file1 = getByTestId('file1.txt');
-    expect(file1).toBeDefined();
+    const app1 = getByTestId('FullJobAttrs');
+    expect(app1).toBeDefined();
 
     // Click on file1.txt and expect the unselect callback to have run
-    file1.click();
-    expect(mockOnUnselect).toHaveBeenLastCalledWith([fileInfo]);
+    app1.click();
+    expect(mockOnUnselect).toHaveBeenLastCalledWith([tapisApp]);
   });
 
-  it('performs select all', () => {
-    const concatenatedResults: Array<Files.FileInfo> = [
-      { ...fileInfo },
-      { ...fileInfo, name: 'file2.txt' },
-    ];
-    (useList as jest.Mock).mockReturnValue({
-      concatenatedResults,
-      isLoading: false,
-      error: null,
-    });
-    const mockOnSelect = jest.fn();
-    const mockOnUnselect = jest.fn();
-    const { getByTestId } = renderComponent(
-      <FileListing
-        systemId={'system'}
-        path={'/'}
-        selectMode={{ mode: 'multi', types: ['dir', 'file'] }}
-        selectedFiles={[fileInfo]}
-        onSelect={mockOnSelect}
-        onUnselect={mockOnUnselect}
-      />
-    );
-    // Find the file1.txt and file2.txt rows
-    const selectAll = getByTestId('select-all');
-    expect(selectAll).toBeDefined();
+  // it('performs select all', () => {
+  //   const concatenatedResults: Array<Files.FileInfo> = [
+  //     { ...fileInfo },
+  //     { ...fileInfo, name: 'file2.txt' },
+  //   ];
+  //   (useList as jest.Mock).mockReturnValue({
+  //     concatenatedResults,
+  //     isLoading: false,
+  //     error: null,
+  //   });
+  //   const mockOnSelect = jest.fn();
+  //   const mockOnUnselect = jest.fn();
+  //   const { getByTestId } = renderComponent(
+  //     <FileListing
+  //       systemId={'system'}
+  //       path={'/'}
+  //       selectMode={{ mode: 'multi', types: ['dir', 'file'] }}
+  //       selectedFiles={[fileInfo]}
+  //       onSelect={mockOnSelect}
+  //       onUnselect={mockOnUnselect}
+  //     />
+  //   );
+  //   // Find the file1.txt and file2.txt rows
+  //   const selectAll = getByTestId('select-all');
+  //   expect(selectAll).toBeDefined();
 
-    // Click on file1.txt and expect the unselect callback to have run
-    selectAll.click();
-    expect(mockOnSelect).toHaveBeenCalledWith(concatenatedResults);
+  //   // Click on file1.txt and expect the unselect callback to have run
+  //   selectAll.click();
+  //   expect(mockOnSelect).toHaveBeenCalledWith(concatenatedResults);
 
-    selectAll.click();
-    expect(mockOnUnselect).toHaveBeenCalledWith(concatenatedResults);
-  });
+  //   selectAll.click();
+  //   expect(mockOnUnselect).toHaveBeenCalledWith(concatenatedResults);
+  // });
 });
