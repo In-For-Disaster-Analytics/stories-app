f5c74852bb10d5b6dea2fd5856863941fb737db7
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-16 11:00:02 -0400

    fix: disable button if I don't have permissions (#34)

--- src/tapis-app/Apps/_components/Toolbar/Toolbar.tsx
+++ src/tapis-app/Apps/_components/Toolbar/Toolbar.tsx
@@ -9,6 +9,7 @@ import { useNotifications } from 'tapis-app/_components/Notifications';
 import { useAppsSelect } from '../AppsContext';
 import UploadModal from 'tapis-app/Apps/_components/Toolbar/UploadModal';
 import ShareModal from './ShareModal';
+import { useTapisConfig } from 'tapis-hooks';
 
 type ToolbarButtonProps = {
   text: string;
@@ -46,79 +47,48 @@ export const ToolbarButton: React.FC<ToolbarButtonProps> = ({
 const Toolbar: React.FC = () => {
   const [modal, setModal] = useState<string | undefined>(undefined);
   const { selectedApps } = useAppsSelect();
-  const { pathname } = useLocation();
-  const systemId = pathname.split('/')[2];
-  const currentPath = pathname.split('/').splice(3).join('/');
   const { download } = useDownload();
   const { add } = useNotifications();
-
-  const { data } = usePermissions({ systemId, path: currentPath });
-  const permission = data?.result?.permission;
-
-  const onDownload = useCallback(() => {
-    selectedApps.forEach((file) => {
-      // const params: DownloadStreamParams = {
-      //   systemId,
-      //   path: file.path ?? '',
-      //   destination: file.name ?? 'tapisfile',
-      // };
-      // const isZip = file.type === 'dir';
-      // if (isZip) {
-      //   params.zip = true;
-      //   params.destination = `${params.destination}.zip`;
-      //   add({ icon: 'data-files', message: `Preparing download` });
-      //   params.onStart = (response: Response) => {
-      //     add({ icon: 'data-files', message: `Starting download` });
-      //   };
-      // }
-      // download(params, {
-      //   onError: isZip
-      //     ? () => {
-      //         add({
-      //           icon: 'data-files',
-      //           message: `Download failed`,
-      //           status: 'ERROR',
-      //         });
-      //       }
-      //     : undefined,
-      // });
-    });
-  }, [selectedApps, add, download, systemId]);
+  const { claims } = useTapisConfig();
+  const isCurrentUser = (username: string) =>
+    username === claims['tapis/username'];
+  const hasPermissions: boolean = selectedApps.every((app) =>
+    app.owner === undefined ? false : isCurrentUser(app.owner)
+  );
 
   const toggle = () => {
     setModal(undefined);
   };
+
   return (
     <div id="file-operation-toolbar">
-      {pathname !== '/files' && (
-        <div className={styles['toolbar-wrapper']}>
-          <ToolbarButton
-            text="Create"
-            icon="add"
-            disabled={false}
-            onClick={() => {
-              setModal('upload');
-            }}
-            aria-label="Add"
-          />
-          <ToolbarButton
-            text="Delete"
-            icon="trash"
-            disabled={selectedApps.length === 0}
-            onClick={() => setModal('delete')}
-            aria-label="Delete"
-          />
-          <ToolbarButton
-            text="Share"
-            icon="globe"
-            disabled={selectedApps.length === 0}
-            onClick={() => setModal('share')}
-          />
-          {modal === 'delete' && <DeleteModal toggle={toggle} />}
-          {modal === 'upload' && <UploadModal toggle={toggle} />}
-          {modal === 'share' && <ShareModal toggle={toggle} />}
-        </div>
-      )}
+      <div className={styles['toolbar-wrapper']}>
+        <ToolbarButton
+          text="Create"
+          icon="add"
+          disabled={false}
+          onClick={() => {
+            setModal('upload');
+          }}
+          aria-label="Add"
+        />
+        <ToolbarButton
+          text="Delete"
+          icon="trash"
+          disabled={selectedApps.length === 0 || !hasPermissions}
+          onClick={() => setModal('delete')}
+          aria-label="Delete"
+        />
+        <ToolbarButton
+          text="Share"
+          icon="globe"
+          disabled={selectedApps.length === 0 || !hasPermissions}
+          onClick={() => setModal('share')}
+        />
+        {modal === 'delete' && <DeleteModal toggle={toggle} />}
+        {modal === 'upload' && <UploadModal toggle={toggle} />}
+        {modal === 'share' && <ShareModal toggle={toggle} />}
+      </div>
     </div>
   );
 };
